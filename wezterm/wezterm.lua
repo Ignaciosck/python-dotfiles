6ebba8e717391dbe23663f5f325a1cc37a0b54a2
ignacio.hrndzlpz@gmail.com <ignacio.hrndzlpz@gmail.com>
2024-09-26 14:08:44 +0200

    wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+
+
+local config = require("wezterm").config_builder()
+
+
+
+config.font =
+
+  wezterm.font('SauceCodePro Nerd Font')
+
+config.font_size = 12
+
+
+
+config.enable_tab_bar = true
+
+config.window_decorations = "RESIZE"
+
+
+
+config.window_background_opacity = 0.85
+
+
+
+return config
+
+
