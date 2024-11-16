f553e51f0ac93805a73b92b10dc50e1405b56642
Ignacio Hernández López <ignacio.hrndzlpz@gmail.com>
2024-11-16 18:45:15 +0100

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,25 +1,173 @@
-local wezterm = require("wezterm")
+--[[local wezterm = require("wezterm")
+
 local config = wezterm.config_builder()
-
-
 config.font =
-
   wezterm.font('JetBrainsMono Nerd Font Mono')
-
 config.font_size = 12
 
-
-
 config.enable_tab_bar = true
-config.front_end = "WebGpu"
 config.window_decorations = "RESIZE"
-
-
+config.color_scheme = 'Cupertino (base16)'
 
 config.window_background_opacity = 0.85
+config.front_end = "WebGpu"
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+config.keys = {
+  -- ... add these new entries to your config.keys table
+  {
+    
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = '-',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
+
+return config]]--
+
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- For example, changing the color scheme:\
+config.enable_tab_bar = true
+config.window_decorations = "RESIZE"
+config.front_end = "WebGpu"
+config.color_scheme = "catppuccin-latte"
+config.font =
+    wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 16
 
 
 
+-- tmux
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
+config.keys = {
+    {
+        mods = "LEADER",
+        key = "c",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        mods = "CTRL",
+        key = "d",
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        mods = "LEADER",
+        key = "b",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "\\",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "-",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "ALT",
+        key = "LeftArrow",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "ALT",
+        key = "DownArrow",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "ALT",
+        key = "UpArrow",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "ALT",
+        key = "RightArrow",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.AdjustPaneSize { "Left", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.AdjustPaneSize { "Right", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.AdjustPaneSize { "Down", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.AdjustPaneSize { "Up", 5 }
+    },
+}
+
+for i = 0, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = wezterm.action.ActivateTab(i),
+    })
+end
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = true
+config.default_prog = {"powershell.exe","-NoLogo"}
+config.tab_and_split_indices_are_zero_based = false
+config.inactive_pane_hsb = {
+  saturation = 1,
+  brightness = 1,
+}
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+    local SOLID_LEFT_ARROW = ""
+    local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+    local prefix = ""
+
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
+
+    if window:active_tab():tab_id() ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    end -- arrow color based on if tab is first pane
+
+    window:set_left_status(wezterm.format {
+        { Background = { Color = "#b7bdf8" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW }
+    })
+end)
+
+-- and finally, return the configuration to wezterm
 return config
-
-
